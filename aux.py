4d8dbb1db10652b8335b889bc15e9c371bed20d9
matijaj <>
2023-04-24 22:48:00 +0200

    histogram generator

--- aux.py
+++ aux.py
@@ -1,5 +1,7 @@
 import json
+import os
 import numpy as np
+
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 with open('by_group.json') as f:
@@ -15,7 +17,31 @@ pca = PCA(n_components=2)
 pca.fit(arr)
 t = pca.transform(arr)
 
-for index,e in enumerate(t):
+for index, e in enumerate(t):
     plt.scatter(e[0], e[1])
     plt.text(e[0], e[1], keys[index])
 plt.show()
+
+
+histograms = dict()
+
+with open('raw.json') as f:
+    data = json.load(f)
+
+for e in data:
+    if e[2] in histograms.keys():
+        histograms[e[2]].append(e[0])
+    else:
+        histograms[e[2]] = [e[0]]
+
+os.mkdir('plots')
+for e in histograms.keys():
+    tmp = np.array(histograms[e])/len(histograms[e])
+    tmp = tmp.mean(axis=0)
+
+    plt.plot(tmp[0], c='r')
+    plt.plot(tmp[1], c='g')
+    plt.plot(tmp[2], c='b')
+    plt.title(e)
+    plt.savefig(f'plots/{e}.png')
+
