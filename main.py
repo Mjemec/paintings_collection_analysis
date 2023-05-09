8ed5daa2b232a7bc95a65ff5ce6b6b8d0d83e5b8
matijaj <>
2023-05-09 16:22:42 +0200

    updated json

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ from sklearn.ensemble import RandomForestClassifier
 from website import img_dir, tag_dir
 
 def debug(msg):
-	print(msg, datetime.datetime.now())
+    print(msg, datetime.datetime.now())
 
 if not os.path.exists('yolov3.weights'):
-	subprocess.Popen(['wget', 'https://pjreddie.com/media/files/yolov3.weights'])
+    subprocess.Popen(['wget', 'https://pjreddie.com/media/files/yolov3.weights'])
 
 if not os.path.exists('yolov3.cfg'):
-	a = subprocess.check_call(['wget', 'https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg'])
+    a = subprocess.check_call(['wget', 'https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg'])
 
 debug('load')
 ds = deeplake.load('hub://activeloop/wiki-art')
@@ -29,112 +29,113 @@ i = 0
 NUMBER_OF_INSTANCES = 20
 debug("randomlist")
 rand_list = random.sample(range(0, len(ds.images)), NUMBER_OF_INSTANCES)
-colors = {}
 x_list = list()
 y_list = list()
 
-color_dict = {
-	"abstract_expressionism": "#FF5733",
-	"action_painting": "#F08080",
-	"analytical_cubism": "#7FFFD4",
-	"art_nouveau_modern": "#6495ED",
-	"baroque": "#8B0000",
-	"color_field_painting": "#FFD700",
-	"contemporary_realism": "#8FBC8F",
-	"cubism": "#FF7F50",
-	"early_renaissance": "#FFDAB9",
-	"expressionism": "#E9967A",
-	"fauvism": "#00FFFF",
-	"high_renaissance": "#FFE4C4",
-	"impressionism": "#3CB371",
-	"mannerism_late_renaissance": "#FFC0CB",
-	"minimalism": "#ADD8E6",
-	"naive_art_primitivism": "#BDB76B",
-	"new_realism": "#FF6347",
-	"northern_renaissance": "#DA70D6",
-	"pointillism": "#CD5C5C",
-	"pop_art": "#00BFFF",
-	"post_impressionism": "#8B008B",
-	"realism": "#87CEFA",
-	"rococo": "#FFA07A",
-	"romanticism": "#FF00FF",
-	"symbolism": "#228B22",
-	"synthetic_cubism": "#FF1493",
-	"ukiyo_e": "#800080"
-}
+times = ["early_renaissance", "high_renaissance", "mannerism_late_renaissance", "northern_renaissance", "baroque",
+         "ukiyo_e", "rococo", "realism", "impressionism", "romanticism", "symbolism", "pointillism",
+         "art_nouveau_modern", "naive_art_primitivism", "post_impressionism", "fauvism", "cubism", "analytical_cubism",
+         "expressionism", "synthetic_cubism", "synthetic_cubism", "expressionism", "color_field_painting",
+         "abstract_expressionism", "action_painting", "pop_art", "contemporary_realism", "new_realism", "minimalism"]
+
+colors_by_time = ['#FFDAB9', '#FFE4C4', '#FFC0CB', '#DA70D6', '#8B0000', '#800080', '#FFA07A', '#87CEFA', '#3CB371',
+                  '#FF00FF', '#228B22', '#CD5C5C', '#6495ED', '#8B008B', '#00FFFF', '#FF7F50', '#7FFFD4', '#E9967A',
+                  '#FF1493', '#FF1493', '#E9967A', '#FFD700', '#FF5733', '#F08080', '#00BFFF', '#8FBC8F', '#FF6347',
+                  '#ADD8E6', "#BDB76B"]
 
 # clear directory with images
 if os.path.isdir(img_dir):
-	shutil.rmtree(img_dir)
+    shutil.rmtree(img_dir)
 os.mkdir(img_dir)
 
 # clear directory with tags
 if os.path.isdir(tag_dir):
-	shutil.rmtree(tag_dir)
+    shutil.rmtree(tag_dir)
 os.mkdir(tag_dir)
 
+by_group_all = dict()
+for time in times:
+    by_group_all[time] = []
+
 for i in rand_list:
-	debug('image #' + str(i))
-	image = ds.images[i].numpy()
-	debug('write image to file')
-	im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	poses, cnt = filters.get_pose_mjeme(im_rgb)
-	cv2.imwrite(img_dir + "/image_" + str(i) + "_pose.png", poses)
-	cv2.imwrite(img_dir + "/image_" + str(i) + ".png", im_rgb)
+    debug('image #' + str(i))
+    image = ds.images[i].numpy()
+    debug('write image to file')
+    im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    poses, cnt = filters.get_pose_mjeme(im_rgb)
+    cv2.imwrite(img_dir + "/image_" + str(i) + "_pose.png", poses)
+    cv2.imwrite(img_dir + "/image_" + str(i) + ".png", im_rgb)
 
-	debug('label')
-	label = ds.labels[i].data()
+    debug('label')
+    label = ds.labels[i].data()
 
-	debug('filter')
-	vector = []
-	img_tag = {}
-	hist = filters.get_histogram(image, False)
+    debug('filter')
+    vector = []
+    img_tag = {}
+    hist = filters.get_histogram(image, False)
 
-	# histogram
-	for j in range(len(hist)):
-		color = hist[j]
-		mean = (color*np.array(range(0, 256))).sum()/color.sum()
-		std = np.std(color)
-		vector.append(mean)
-		vector.append(std)
-		img_tag["color_" + str(j) + "_mean"] = mean
-		img_tag["color_" + str(j) + "_std"] = std
-	print("")
+    # histogram
+    for j in range(len(hist)):
+        color = hist[j]
+        mean = (color*np.array(range(0, 256))).sum()/color.sum()
+        std = np.std(color)
+        vector.append(mean)
+        vector.append(std)
+        img_tag["color_" + str(j) + "_mean"] = mean
+        img_tag["color_" + str(j) + "_std"] = std
+    print("")
 
-	# line count
-	line_count = filters.get_lines_count(image)
-	vector.append(line_count)
-	img_tag["line_count"] = line_count
+    # line count
+    line_count = filters.get_lines_count(image)
+    vector.append(line_count)
+    img_tag["line_count"] = line_count
 
-	# face count
-	face, face_count = filters.get_face_count(im_rgb)
-	vector.append(face_count)
-	img_tag["face_count"] = face_count
+    # face count
+    face, face_count = filters.get_face_count(im_rgb)
+    vector.append(face_count)
+    img_tag["face_count"] = face_count
 
-	# people count
-	vector.append(cnt)
-	img_tag["people_count"] = cnt
+    # people count
+    vector.append(cnt)
+    img_tag["people_count"] = cnt
 
-	# contour count
-	contour_count = filters.get_contour_count(image)
-	vector.append(contour_count)
-	img_tag["contour_count"] = contour_count
+    # contour count
+    contour_count = filters.get_contour_count(image)
+    vector.append(contour_count)
+    img_tag["contour_count"] = contour_count
 
-	x_list.append(vector)
-	time_period = label['text'][0]
-	y_list.append(time_period)
-	img_tag["time_period"] = time_period
+    x_list.append(vector)
+    time_period = label['text'][0]
+    y_list.append(time_period)
+    img_tag["time_period"] = time_period
 
-	# serializing json
-	tag_json = json.dumps(img_tag, indent=4)
+    # serializing json
+    tag_json = json.dumps(img_tag, indent=4)
 
-	# write image tag in json format
-	with open(tag_dir + "/image_" + str(i) + ".json", "w") as tag_file:
-		tag_file.write(tag_json)
+    # write image tag in json format
+    with open(tag_dir + "/image_" + str(i) + ".json", "w") as tag_file:
+        tag_file.write(tag_json)
 
-clf = RandomForestClassifier(n_estimators=NUMBER_OF_INSTANCES)
-clf.fit(x_list, y_list)
+    by_group_all[time_period].append(vector)
 
-# Calculate feature importance scores
-importance_scores = clf.feature_importances_
-print(importance_scores)
+with open('by_group_all.json', 'w+') as f:
+    data = json.dumps(by_group_all)
+    f.write(data)
+
+do_json = False
+if do_json:
+    by_group_new = dict()
+    for e in times:
+        tmp = by_group_all[e]
+        h = np.array(tmp)
+        if len(tmp):
+            by_group_new[e] = list(h.mean(axis=0))
+
+    with open('by_group_new.json', 'w+') as f:
+        data = json.dumps([times, by_group_new], indent=4)
+        f.write(data)
+
+
+
+
+
