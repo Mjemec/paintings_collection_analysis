0658bafa4a110759a852500a38a9a1a1cd8f3160
jan146 <anurjan@gmail.com>
2023-05-16 23:41:29 +0200

    Added background to images

--- website_client/src/charts.js
+++ website_client/src/charts.js
@@ -37,69 +37,69 @@ const id2time_peroid = [
 
 export function initButtons() {
 
-  let btnFaces = document.getElementById("btn-faces");
-  let btnPoses = document.getElementById("btn-poses");
-  let btnLines = document.getElementById("btn-lines");
-  let btnContours = document.getElementById("btn-contours");
+	let btnFaces = document.getElementById("btn-faces");
+	let btnPoses = document.getElementById("btn-poses");
+	let btnLines = document.getElementById("btn-lines");
+	let btnContours = document.getElementById("btn-contours");
 
-  // console.log(btnFaces);
-  // console.log(btnPoses);
-  // console.log(btnLines);
-  // console.log(btnContours);
+	// console.log(btnFaces);
+	// console.log(btnPoses);
+	// console.log(btnLines);
+	// console.log(btnContours);
 
-  btnFaces.onclick = () => loadChart("faces");
-  btnPoses.onclick = () => loadChart("poses");
-  btnLines.onclick = () => loadChart("lines");
-  btnContours.onclick = () => loadChart("contours");
+	btnFaces.onclick = () => loadChart("faces");
+	btnPoses.onclick = () => loadChart("poses");
+	btnLines.onclick = () => loadChart("lines");
+	btnContours.onclick = () => loadChart("contours");
 
 }
 
 export function loadChart(chartStr) {
 
-  let bannerTitle = document.getElementById("banner-title");
-  if (bannerTitle)
-    bannerTitle.innerHTML = "<b>Average</b> number of <b>" + chartStr + "</b> by time period";
+	let bannerTitle = document.getElementById("banner-title");
+	if (bannerTitle)
+		bannerTitle.innerHTML = "<b>Average</b> number of <b>" + chartStr + "</b> by time period";
 
 	console.log("loading chart: " + chartStr);
 
 	let promise = $.get(serverUrl + "/chart/" + chartStr);
-  if (chart)
-    chart.destroy();
+	if (chart)
+		chart.destroy();
 
 	promise.done((data) => {
 		chart = new Chart(
 			document.getElementById('barChart'),
 			{
-			type: 'bar',
-			data: {
-				labels: data.map(row => snakeCase2properCase(row.time_period)),
-				datasets: [
-				{
-					label: "Average number of " + chartStr + " by time period",
-					data: data.map(row => row.value)
-				}
-				]
-			},
-			options: {
-        plugins: {
-          legend: {
-            display: false
-          }
-        },
-				maintainAspectRatio: false,
-				indexAxis: 'y',
-				onClick: (e) => {
-					const canvasPosition = getRelativePosition(e, chart);
-		
-					// Substitute the appropriate scale IDs
-					const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
-					const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
-					// console.log(dataY);
-					// console.log("Clicked column: " + dataY);
-					showImages(id2time_peroid[dataY]);
+				type: 'bar',
+				data: {
+					labels: data.map(row => snakeCase2properCase(row.time_period)),
+					datasets: [
+					{
+						label: "Average number of " + chartStr + " by time period",
+						data: data.map(row => row.value)
+					}
+					]
+				},
+				options: {
+					plugins: {
+						legend: {
+							display: false
+						}
+					},
+					maintainAspectRatio: false,
+					indexAxis: 'y',
+					onClick: (e) => {
+						const canvasPosition = getRelativePosition(e, chart);
+			
+						// Substitute the appropriate scale IDs
+						const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+						const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+						// console.log(dataY);
+						// console.log("Clicked column: " + dataY);
+						showImages(id2time_peroid[dataY]);
 
+					}
 				}
-			}
 			}
 		);
 	});
